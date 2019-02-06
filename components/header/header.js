c1a788a31de7d62aa3aec2eddc87280c28efcac5
Giacomo Benati <giacomobenati@mailbox.org>
2019-02-06 12:07:30 +0100

    added hover behviour on best selling cards + fixed header resizing behaviour

--- components/header/header.js
+++ components/header/header.js
@@ -1,41 +1,53 @@
 const unorderedLists = document.querySelectorAll('header ul');
+const mainHeaderItems = unorderedLists[0];
+const moreMenuItems = unorderedLists[1];
+const topHeaderItems = unorderedLists[2];
 const icons = document.querySelectorAll('header span');
 const header = document.querySelector('header')
 const topHeader = document.querySelector('.top-header');
 const navHeader = document.querySelector('nav');
 
 let viewportWidth;
-
 let setViewportWidth = function () {
 	viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 }
 
-const mainList = document.createElement('div');
-mainList.classList.add('main-list');
-mainList.appendChild(unorderedLists[0]);
-mainList.appendChild(unorderedLists[1]);
-mainList.appendChild(unorderedLists[2]);
+
+const mainListCreation = () => {
+    mainList = document.createElement('div');
+    mainList.classList.add('main-list');
+    mainList.appendChild(mainHeaderItems);
+    mainList.appendChild(moreMenuItems);
+    mainList.appendChild(topHeaderItems);
+    
+}
 
 setViewportWidth();
 if (viewportWidth < 1000) {
+    mainListCreation();
     header.appendChild(mainList);
 } else {        
-    topHeader.appendChild(unorderedLists[2]);
-    navHeader.appendChild(unorderedLists[0]);
+    topHeader.appendChild(topHeaderItems);
+    navHeader.appendChild(mainHeaderItems);
 }
 
 window.addEventListener('resize', () => {
     setViewportWidth();
     
     if (viewportWidth < 1000) {
+        if (topHeader.contains(topHeaderItems)) {
+            topHeader.removeChild(topHeaderItems);
+            navHeader.removeChild(mainHeaderItems);
+        }
+        mainListCreation();
         header.appendChild(mainList);
         
     } else {
         if (header.contains(mainList)) {
             header.removeChild(mainList);
         }        
-        topHeader.appendChild(unorderedLists[2]);
-        navHeader.appendChild(unorderedLists[0]);
+        topHeader.appendChild(topHeaderItems);
+        navHeader.appendChild(mainHeaderItems);
     }
 })
 
---
+++ home/home.js
@@ -0,0 +1,28 @@
+class CardAnimation {
+    
+    constructor(card) {
+        const cover = document.createElement('div');
+        const heart = document.createElement('span');
+        cover.classList.add('cover');
+        heart.innerHTML = '&#xe089;';
+        heart.classList.add('heart');
+        this.card = card;
+        this.cover = cover;
+        this.heart = heart;
+
+        this.cover.appendChild(this.heart);
+        this.card.appendChild(this.cover);
+
+        this.card.addEventListener('mouseover', () => this.cover.classList.add('open'));
+        this.card.addEventListener('mouseout', () => this.cover.classList.remove('open'))
+    }
+
+}
+
+
+const cardsBestSelling = document.querySelectorAll('.best-selling .card figure');
+
+cardsBestSelling.forEach(card => new CardAnimation(card));
+
+
+
